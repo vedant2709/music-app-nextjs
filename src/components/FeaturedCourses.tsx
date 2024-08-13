60dd8c7690fa037df472cf500512b506b7f202d5
Vedant Chaudhary <vedantchaudhary2002@gmail.com>
2024-08-13 17:36:29 +0530

    started with infinite slider

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -33,15 +33,20 @@ function FeaturedCourses() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {featuredCourses.map((course: Course) => (
             <div key={course.id} className="flex justify-center">
-              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-zinc-900 dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
                 <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                  <p className="text-lg sm:text-xl text-neutral-200 mt-4 mb-2 dark:text-neutral-200">
                     {course.title}
                   </p>
-                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                  <p className="text-sm text-neutral-400 dark:text-neutral-400 flex-grow">
                     {course.description}
                   </p>
-                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
+                  <Link
+                    href={`/courses/${course.slug}`}
+                    className="text-black mt-4 py-1 px-3 bg-white rounded-md hover:bg-gray-500 hover:text-white transition duration-200"
+                  >
+                    Learn More
+                  </Link>
                 </div>
               </BackgroundGradient>
             </div>
